d4d584fa9276b76acc6a79fbeb6903030a06c273
alexisg78 <galexis.isi78gmail.com>
2025-01-28 15:05:43 -0300

    Actualizaciones minimas

---
+++ src/app/tickets/interfaces/ticket.interface.ts
@@ -0,0 +1,13 @@
+export interface Tickets {
+  "fecha": "string",
+  "titulo": "string",
+  "textoreclamo": "string",
+  "nombreoperador": "string",
+  "codigoarea": 0,
+  "nombre": "string",
+  "fecharequerida": "string",
+  "estado": "string",
+  "color": 0,
+  "codigoppal": 0,
+  "codigoestado": 0
+}
