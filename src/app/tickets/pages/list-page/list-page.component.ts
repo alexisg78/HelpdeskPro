7103f68c52ee9a1547d8d6bcd6c31d230f35a7d4
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-03-15 17:49:17 -0300

    Solucion de bugs en filtros

--- src/app/tickets/pages/list-page/list-page.component.ts
+++ src/app/tickets/pages/list-page/list-page.component.ts
@@ -22,14 +22,12 @@ export class ListPageComponent implements OnInit {
   public empresas!: Empresa[];
   public areas!: Area[];
   public filtros?: Filtros= {};
-  public tickets_filtrados?: HelpDesk[] = [];
-  public filtra: boolean = false
-  public selectedArea= {}
-  public selectedEmpresa= {}
+  public selectedArea?: Area;
+  public selectedEmpresa?: Empresa;
+
   constructor(private ticketsService: TicketsService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    // Carga las empresas y áreas
     forkJoin({
       empresas: this.ticketsService.getEmpresa(),
       areas: this.ticketsService.getArea(),
@@ -37,22 +35,44 @@ export class ListPageComponent implements OnInit {
       this.empresas = empresas;
       this.areas = areas;
 
-      // Variable para almacenar la selección
+      // Seteo Areas y Empresas
       this.selectedArea = this.areas[0]
       this.selectedEmpresa = this.empresas[0]
 
       this.getActualiza();
-
-      // localStorage.setItem('Filtros', JSON.stringify(this.filtros));
-
     });
-
-    // this.getActualiza();
   }
 
-  selectRow(item: any) {
-    this.selectedTicket = item;
-    this.ticketSeleccionado = item;
+  getActualiza() {
+    // Cargo los tickets desde el servicio
+    this.ticketsService.getTickets().pipe(take(1))
+      .subscribe(tickets => {
+        const gFiltros = localStorage.getItem('Filtros');
+        const gTicket_seleccionado =  localStorage.getItem('Ticket_Seleccionado')
+
+        this.tickets = tickets;
+        this.tickets_backup = tickets;
+
+        if ( gTicket_seleccionado ){
+          const tSelect= JSON.parse(gTicket_seleccionado)
+          this.selectRow(tSelect);
+          localStorage.removeItem('Ticket_Seleccionado');
+        }else {
+          this.selectRow(this.tickets[0])
+          this.selectedTicket =  this.tickets[0];
+        }
+
+        if ( gFiltros ){
+          this.getFiltros(gFiltros);
+          localStorage.removeItem('Filtros');
+          return;
+        }
+
+        let cod_emp_filtro= this.selectedEmpresa?.codigo || 0
+        let cod_area_filtro= this.selectedArea?.codigo || 0
+
+        this.filterByEmpresaYArea(cod_emp_filtro, cod_area_filtro)
+      });
   }
 
   getTicket(item: HelpDesk, id: number) {
@@ -62,61 +82,27 @@ export class ListPageComponent implements OnInit {
     };
 
     this.router.navigate([`helpdesk/details`, id], { queryParams: { data: JSON.stringify(this.filtros) } });
-    this.selectRow(item);
+    localStorage.setItem('Ticket_Seleccionado', JSON.stringify(item));
 
-    // Guarda los filtros en localStorage
+    if (this.selectedEmpresa?.codigo === 0 && this.selectedArea?.codigo === 0) return;
+
+    if ( this.selectedEmpresa?.codigo === 0 || this.selectedArea?.codigo === 0 ) {
+
+      if ( this.selectedEmpresa?.codigo && this.selectedArea?.codigo ){
+        this.filterByEmpresaYArea(this.selectedEmpresa?.codigo, this.selectedArea?.codigo)
+      }
+
+      this.filtros = {
+        codEmpresa: this.selectedEmpresa?.codigo,
+        codArea: this.selectedArea?.codigo
+      };
+
+      localStorage.setItem('Filtros', JSON.stringify(this.filtros));
+      this.selectRow(item);
+      return;
+    }
+
     localStorage.setItem('Filtros', JSON.stringify(this.filtros));
-
-  }
-
-  getActualiza() {
-    // Carga los tickets desde el servicio
-    this.ticketsService.getTickets().pipe(take(1))
-      .subscribe(tickets => {
-        this.tickets = tickets;
-        this.tickets_backup = tickets;
-        this.selectedTicket = this.tickets[0];
-
-        // if (this.filtros?.codArea === 0 && this.filtros.codEmpresa === 0) return;  FUNCIONA - VER
-
-        const gFiltros = localStorage.getItem('Filtros');
-        if ( gFiltros ){
-          this.getFiltros(gFiltros);
-          return;
-        }else{
-          this.limpiarFiltros(0)
-          console.log('Limpia Filtros')
-        }
-
-      });
-  }
-
-  filterByEmpresa(event: CustomEvent) {
-    const codEmpresa = event.detail.value.codigo;
-    this.empresaSeleccionada = this.empresas.filter(e => e.codigo === codEmpresa);
-    this.selectedEmpresa= this.empresaSeleccionada
-    this.filterByEmpresaYArea(codEmpresa, this.areaSeleccionada?.[0]?.codigo || 0);
-  }
-
-  filterByArea(event: CustomEvent) {
-    const codArea = event.detail.value.codigo;
-    this.areaSeleccionada = this.areas.filter(a => a.codigo === codArea);
-    this.tickets = this.tickets_backup.filter(t => t.area.codigo === event.detail.value.codigo);
-    this.selectedArea= this.areaSeleccionada
-    this.filterByEmpresaYArea(this.empresaSeleccionada?.[0]?.codigo || 0, codArea);
-  }
-
-  filterByEmpresaYArea(codEmpresa: number, codArea: number) {
-
-    this.tickets = this.tickets_backup.filter(t =>
-        (codEmpresa === 0 || t.empresa.codigo === codEmpresa) &&
-        (codArea === 0 || t.area.codigo === codArea)
-    );
-
-    this.tickets_filtrados = [...this.tickets];
-    console.log('Tickets Filtrados: ', this.tickets_filtrados)
-
-    localStorage.removeItem('Filtros');
   }
 
   getFiltros(gFiltros:any) {
@@ -125,13 +111,45 @@ export class ListPageComponent implements OnInit {
       this.filtros = filtrosParsed;
       const {codEmpresa, codArea} = this.filtros
 
-      if ( codEmpresa && codArea ){
-        this.selectedArea= this.areas.filter( a => a.codigo === codArea )
-        this.selectedEmpresa= this.empresas.filter( e => e.codigo === codEmpresa )
-        this.filterByEmpresaYArea(codEmpresa, codArea)
-      }
-    }
+      let area_select= this.areas.filter( a => a.codigo === codArea )
+      let emp_select= this.empresas.filter( e => e.codigo === codEmpresa )
 
+      this.selectedArea= area_select[0];
+      this.selectedEmpresa= emp_select[0];
+
+      this.filterByEmpresaYArea(this.selectedEmpresa.codigo , this.selectedArea.codigo )
+    }
+  }
+
+  // Filtro Empresa
+  filterByEmpresa(event: CustomEvent) {
+    const codEmpresa = event.detail.value.codigo;
+    this.empresaSeleccionada = this.empresas.filter(e => e.codigo === codEmpresa);
+    this.selectedEmpresa= this.empresaSeleccionada[0]
+
+    this.filterByEmpresaYArea(codEmpresa, this.selectedArea?.codigo || 0);
+  }
+
+  // Filtro Area
+  filterByArea(event: CustomEvent) {
+    const codArea = event.detail.value.codigo;
+    this.areaSeleccionada = this.areas.filter(a => a.codigo === codArea);
+    this.selectedArea= this.areaSeleccionada[0]
+
+    this.filterByEmpresaYArea(this.selectedEmpresa?.codigo || 0, codArea);
+  }
+
+  // Filtro Empresa y Area
+  filterByEmpresaYArea(codEmpresa: number, codArea: number) {
+    this.tickets = this.tickets_backup.filter(t =>
+        (codEmpresa === 0 || t.empresa.codigo === codEmpresa) &&
+        (codArea === 0 || t.area.codigo === codArea)
+    );
+  }
+
+  selectRow(item: any) {
+    this.selectedTicket = item;
+    this.ticketSeleccionado = item;
   }
 
   limpiarFiltros(codigo: number){
